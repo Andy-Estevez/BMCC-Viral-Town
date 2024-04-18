43d8f37453fa4dde4b06cae4a10138405e708d70
Andy Estevez <150390466+Andy-Estevez@users.noreply.github.com>
2024-04-18 15:07:46 -0400

    Feat: City class.
    Added initial city class.

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class City : MonoBehaviour
+{
+    private int population;
+    private int healthy;
+    private int infected;
+    private int GDP;
+    private Buildings buildings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Generate City
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
+struct Buildings
+{
+    // ADD RESIDENTIAL & ABSTRACT LIST/ARRAY
+}
