c7d8755af640cf4bf2b1e7c461367c421a56f7fe
eselmsenavarank <varankeslemsena@gmail.com>
2024-04-29 15:45:58 -0400

    The Final Working Spawning Algorithm

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -11,53 +11,74 @@ public class BuildingSpawner : MonoBehaviour
     [SerializeField] private GameObject[] residentalBuildings; // Array of residental building prefabs
     [SerializeField] private GameObject[] commercialBuildings; // Array of commercial building prefabs
 
-    [SerializeField] private Transform officeZone;     // Array of all the zone transforms
-    [SerializeField] private Transform residentalZone;
-    [SerializeField] private Transform commercialZone;
+    [SerializeField] private Transform[] officeBorder;// Array of all the zone transforms
+    [SerializeField] private Transform residentalBorder;
+    [SerializeField] private Transform commercialBorder;
+
+    [SerializeField] private Transform officeContainer;     // Array of all the zone transforms
+    [SerializeField] private Transform residentalContainer;
+    [SerializeField] private Transform commercialContainer;
+
+
     //[SerializeField] private int[] buildingsPerZone;    // Number of buildings to spawn in each zone
 
     void Start()
     {
 
-        SpawnBuildings(residentalBuildings, residentalZone, 15);
-        SpawnBuildings(officeBuildings, officeZone, 10);
-        
-        SpawnBuildings(commercialBuildings, commercialZone, 7);
+        SpawnBuildings(residentalBuildings, residentalContainer, residentalBorder, 1, 18);
+        SpawnBuildings(officeBuildings, officeContainer, officeBorder[0], 4/5, 4);
+        SpawnBuildings(officeBuildings, officeContainer, officeBorder[1], 4 / 5, 4);
+        SpawnBuildings(officeBuildings, officeContainer, officeBorder[2], 4 / 5, 4);
+        SpawnBuildings(commercialBuildings, commercialContainer, commercialBorder, 4/5, 18);
 
     }
 
-    void SpawnBuildings(GameObject[] prefabs, Transform zone, int amount)
+    void SpawnBuildings(GameObject[] prefabs, Transform zoneContainer, Transform zoneBorder, float streetWidth, int amount)
     {
-        BoxCollider zoneCollider = zone.GetComponent<BoxCollider> ();
-        Bounds zoneBounds = zoneCollider.bounds;
+        
+        float buildingWidth = prefabs[0].transform.localScale.x;
 
+        BoxCollider2D collid = zoneBorder.GetComponent<BoxCollider2D>();
 
-        // Define the spacing between buildings and the number of buildings per row
-        float buildingSpacing = 10f; // Adjust this based on the size of your building prefabs and desired street width
-        int buildingsPerRow = Mathf.FloorToInt((zoneBounds.size.x - buildingSpacing) / (prefabs[0].transform.localScale.x + buildingSpacing));
+        // The starting position for the first building. Here we set the starting pos to left upper corner of the zone.
+        Vector3 nextPosition = new Vector3(collid.bounds.min.x, collid.bounds.max.y, 0);
 
-        Vector3 startPosition = new Vector3(zoneBounds.min.x, 0, zoneBounds.min.z); // Ground level assumed as '0' for y-axis
+        // The distance to the next building spot (building width plus street width)
+        Vector3 offset = new Vector3(buildingWidth + streetWidth, buildingWidth + streetWidth, 0);
 
-        int buildingCount = 0;
-        float posX, posZ;
+        // Calculate the area of the zone container using collider bounds
+        float zoneWidth = collid.bounds.size.x;
 
-        // Loop to spawn buildings row by row
-        for (int i = 0; buildingCount < amount && i < (amount / buildingsPerRow) + 1; i++)
+        // Handle potential division by zero (assuming 'amount' is number of buildings)
+        if (amount == 0)
         {
-            for (int j = 0; j < buildingsPerRow && buildingCount < amount; j++)
+            Debug.LogError("Number of buildings cannot be zero!");
+            return; // Or handle the case with a default value
+        }
+
+        // Calculate the number of buildings per row
+        int buildingsPerRow = Mathf.FloorToInt((zoneWidth / (buildingWidth + streetWidth))+1); 
+
+        for (int i = 0; i < amount; i++)
+        {
+            // Calculate new position for each building in a row
+            if (i % buildingsPerRow == 0 && i != 0) // After a row is complete, reset to next row
             {
-                posX = startPosition.x + j * (prefabs[0].transform.localScale.x + buildingSpacing);
-                posZ = startPosition.z + i * (prefabs[0].transform.localScale.z + buildingSpacing);
-
-                // Select a random prefab from the array
-                GameObject prefabToSpawn = prefabs[Random.Range(0, prefabs.Length)];
-
-                // Instantiate building at calculated position
-                Vector3 spawnPosition = new Vector3(posX, startPosition.y, posZ);
-                Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity, zone);
-
-                buildingCount++;
+                nextPosition.x = collid.bounds.min.x; // Reset to first column
+                nextPosition.y -= offset.y; // Move to next row
+                Debug.Log("in the loop");
             }
+            else if (i != 0)
+            {
+                nextPosition.x += offset.x; // Move to next column
+            }
+
+
+            // Instantiate the building at nextPosition
+            GameObject newBuilding = Instantiate(prefabs[Random.Range(0, prefabs.Length)], nextPosition, Quaternion.identity);
+
+            // Parent it to the zone container so it's organized correctly in the scene
+            newBuilding.transform.SetParent(zoneContainer);
         }
     }
 
