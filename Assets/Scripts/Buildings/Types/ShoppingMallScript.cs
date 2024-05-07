7d304778637b627786ae59529f6dd03dd83ad653
Ayana <122323256+ayamonta@users.noreply.github.com>
2024-05-07 19:13:00 -0400

    attached buildingType scripts to building prefabs

--- Assets/Scripts/Buildings/Types/ShoppingMallScript.cs
+++ Assets/Scripts/Buildings/Types/ShoppingMallScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MallScript : BuildingTemplate
+public class ShoppingMallScript : BuildingTemplate
 {
     // Start is called before the first frame update
     void Start()
